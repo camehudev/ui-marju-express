57a86e356206800903fb19993f6797b069ba3c7e
Carlos Humberto <camehudev@gmail.com>
2026-09-24 05:03:14 -0300

    Atualizar o main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 import flet as ft
 import flet_webview as fwv
 
-
 def main(page: ft.Page):
+    # 1. Adicionar o manipulador de permissões na overlay da página
+    ph = ft.PermissionHandler()
+    page.overlay.append(ph)
+
     page.add(
         ft.SafeArea(
             expand=True,
             content=fwv.WebView(
-              url="https://pessoal-web-marju-express.sjj3wv.easypanel.host/",
+                url="https://pessoal-web-marju-express.sjj3wv.easypanel.host/",
                 on_page_started=lambda _: print("Page started"),
                 on_page_ended=lambda _: print("Page ended"),
                 on_web_resource_error=lambda e: print("WebView error:", e.data),
@@ -15,7 +18,6 @@ def main(page: ft.Page):
             ),
         )
     )
-
 
 if __name__ == "__main__":
     ft.run(main)
