3c45cc6e6f3b446052562dcf3b7d7ea3893be3b3
Carlos Humberto <camehudev@gmail.com>
2026-09-24 04:11:56 -0300

    Atualizar o main.py

--- src/main.py
+++ src/main.py
@@ -1,26 +1,21 @@
 import flet as ft
-import flet_webview as ftwv
+import flet_webview as fwv
+
 
 def main(page: ft.Page):
-    # Configurações da Janela / App
-    page.title = "Marju Express"
-    page.theme_mode = ft.ThemeMode.LIGHT
-    
-    # Remove margens padrão para o WebView ocupar 100% da tela
-    page.padding = 0
-
-    # URL do seu sistema em produção (Angular, etc.)
-    # URL_SISTEMA = "https://pessoal-web-marju-express.sjj3wv.easypanel.host/"
-
-    # Criação do componente WebView (sem barra de progresso)
-    webview = ftwv.WebView(
-        url="https://pessoal-web-marju-express.sjj3wv.easypanel.host/",
-        expand=True,  # Ocupa todo o espaço disponível na tela
-        on_web_resource_error=lambda e: print("Erro ao carregar página:", e.data),
+    page.add(
+        ft.SafeArea(
+            expand=True,
+            content=fwv.WebView(
+              url="https://pessoal-web-marju-express.sjj3wv.easypanel.host/",
+                on_page_started=lambda _: print("Page started"),
+                on_page_ended=lambda _: print("Page ended"),
+                on_web_resource_error=lambda e: print("WebView error:", e.data),
+                expand=True,
+            ),
+        )
     )
 
-    # Adiciona apenas o WebView diretamente na página
-    page.add(webview)
 
 if __name__ == "__main__":
     ft.run(main)
